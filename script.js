927bec266a2b75d7244e4865d34183c9949c4d83
feliperalves21 <feliperochaalves21@gmail.com>
2024-08-22 22:48:11 -0300

    Agora a criptografia só é feita se o usuário digitar letras minúsculas sem acentos e também não utilizar caracteres especiais

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
-function regras_especiais(texto){
-    texto = texto.toLowerCase();
-    texto = texto.normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/[^a-z0-9,.\s]/g, '');
-    return texto;
+/* 
+Regras:
+- Deve funcionar apenas com letras minúsculas
+- Não devem ser utilizados letras com acentos nem caracteres especiais
+- Deve ser possível converter uma palavra para a versão criptografada e também retornar uma palavra criptografada para a versão original.
+*/
+
+function isValidText(texto){
+    let regras_especiais = /^[a-z,.\s]+$/g;
+    return regras_especiais.test(texto);
 }
 
-function criptografar() {
+function criptografar() {    
     //Pegando o que foi digitado no textarea.
-    let campo_criptografar = document.getElementById('criptografar');
-    let texto_descriptografado = campo_criptografar.value;
-    //Adaptando o que usuário digitou para remover acentos, caracteres especiais e deixar tudo em minúsculo.
-    texto_descriptografado = regras_especiais(texto_descriptografado);
-    //Fazendo a criptografia de acordo com as chaves
-    let texto_criptografado = texto_descriptografado.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
-    //Preenchendo o campo de resultado após fazer a criptografia do texto
-    document.getElementById("descriptografar").value = texto_criptografado;
+    let campo_criptografar = document.getElementById('area_de_texto').value;
+    //Verificando se o que usuário digitou está correto de acordo com as regras
+    if (isValidText(campo_criptografar)) {
+        //Fazendo a criptografia de acordo com as chaves
+        let texto_criptografado = campo_criptografar.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
+        //Preenchendo o campo de resultado após fazer a criptografia do texto
+        document.getElementById("resultado").value = texto_criptografado;
+    }
+    else{
+        alert("Apenas letras minúsculas e sem acento.")
+    } 
 }
 
 function descriptografar() {
-    let campo_criptografar = document.getElementById('criptografar');
-    let texto_criptografado = campo_criptografar.value;
-    texto_criptografado = regras_especiais(texto_criptografado);
-    //Fazendo a descriptografia. O processo é simples, apenas o inverso da criptografia.
-    let texto_descriptografado = texto_criptografado.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
-    document.getElementById("descriptografar").value = texto_descriptografado;
+    let campo_criptografar = document.getElementById('area_de_texto').value;
+    if (isValidText(campo_criptografar)) {
+        //Fazendo a descriptografia. O processo é simples, apenas o inverso da criptografia.
+        let texto_descriptografado = campo_criptografar.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
+        document.getElementById("resultado").value = texto_descriptografado;
+    }
+    else{
+        alert("Apenas letras minúsculas e sem acento.");
+    } 
 }
 
 function copiar(){
@@ -30,3 +42,4 @@ function copiar(){
     navigator.clipboard.writeText(texto_copiado);
     alert("Texto copiado!");
 }
+
